05e4c88be77c2750ec9d9905ba16042e2036b2cf
microedet <microedet@microedet.com>
2021-04-19 19:08:36 +0200

    configuracion del panel de administración

--- core/admin.py
+++ core/admin.py
@@ -4,17 +4,29 @@ from django.contrib import admin
 # Register your models here.
 from .models import Cliente,Especialista,Usuario,Mensaje,Cita
 
-'''class ClienteAdmin(admin.ModelAdmin):
-    readline
-admin.site.register(Cliente,ClienteAdmin)
+class ClienteAdmin(admin.ModelAdmin):
+    #readline
+    list_display = ('dni','nombre','apellido')
+    ordering = ('apellido',)
+    search_fields = ('nombre','dni')
+
+
 
 class EspecialistaAdmin(admin.ModelAdmin):
-    readline
-admin.site.register(Especialista,EspecialistaAdmin)'''
+    #readline
+    list_display = ('dni','nombre','apellido')
+    ordering = ('apellido',)
+    search_fields = ('nombre','dni')
+    #list_filter
 
 
-admin.site.register(Cliente)
-admin.site.register(Especialista)
+
+admin.site.register(Especialista,EspecialistaAdmin)
+admin.site.register(Cliente,ClienteAdmin)
+
+
+#admin.site.register(Cliente)
+#admin.site.register(Especialista)
 admin.site.register(Usuario)
 admin.site.register(Mensaje)
 admin.site.register(Cita)
--- core/models.py
+++ core/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Cliente (models.Model):
-    actions_on_bottom=True
     id=models.AutoField(verbose_name="idCLiente",primary_key=True)
     dni=models.CharField(max_length=10)
     nombre = models.CharField(max_length=30)
